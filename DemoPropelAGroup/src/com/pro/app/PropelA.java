ffd9d055435cfd768d8fc753aa1b2685aba851f9
ashakrishnan1997 <ashakrishnan1997@gmail.com>
2020-02-26 13:15:54 +0530

    asha second commit

--- DemoPropelAGroup/src/com/pro/app/PropelA.java
+++ DemoPropelAGroup/src/com/pro/app/PropelA.java
@@ -5,6 +5,9 @@ public class PropelA {
 	public static void main(String[] args) {
 		
 		System.out.println("Annie");
+	   System.out.println("*****");
+	   System.out.println("asha");
+	   System.out.println("///////");
 	}
 
 }
